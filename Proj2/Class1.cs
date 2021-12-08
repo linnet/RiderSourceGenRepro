1ab126f371233f05f8a80de55e7240635df72b43
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2021-12-08 19:04:41 +0100

    Simple repro

---
+++ Proj2/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Proj2;
+
+public class Class1
+{
+    public void Foo()
+    {
+        var foo = Proj1.SourceGenerated.Proj1Generated.Foo;
+    }
+}
---
+++ SourceGen/ExampleSourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGenReproFirst
+{
+    [Generator]
+    public class ExampleSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var assemblyName = context.Compilation.AssemblyName;
+            var sourceText =
+                SourceText.From(
+                    $"namespace {assemblyName}.SourceGenerated {{ public class {assemblyName}Generated {{ public static string Foo = \"Bar\"; }} }}", Encoding.UTF8);
+            context.AddSource("Generated.cs", sourceText);
+        }
+    }
+}
